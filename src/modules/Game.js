b3d3c004e1633cc8e94794f0983854c82745481d
Daniel Hjartland <squashical@gmail.com>
2022-11-05 20:34:57 +0100

    Add functionality for rendering computer ships visible for testing

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -22,19 +22,18 @@ class Game {
     const p1Board = new Gameboard();
     const p2Board = new Gameboard();
     UI.initPlacementListeners();
-    // place player ships
     p1Board.placeShip(new Ship('Battleship', 4), 0, 0, 'horizontal');
     p1Board.placeShip(new Ship('Cruiser', 3), 1, 0, 'horizontal');
     p1Board.placeShip(new Ship('Submarine', 3), 2, 0, 'horizontal');
     p1Board.placeShip(new Ship('Destroyer', 2), 3, 0, 'horizontal');
-    // UI.removePlacementListeners();
     p2Board.placeShip(new Ship('Battleship', 4), 0, 0, 'horizontal');
     p2Board.placeShip(new Ship('Cruiser', 3), 2, 0, 'horizontal');
     p2Board.placeShip(new Ship('Submarine', 3), 4, 3, 'horizontal');
     p2Board.placeShip(new Ship('Destroyer', 2), 6, 5, 'vertical');
-    UI.renderPlayerShips(p1Board, 'player');
-    UI.renderPlayerShips(p2Board, 'computer')
+    UI.renderPlayerShips(p1Board);
+    UI.renderComputerships(p2Board)
     UI.initAttackListeners(p1, p2Board, userInterface.computerReferenceBoard);
+    UI.removeEventListenersBoardSquares();
     UI.renderBoard(userInterface.computerReferenceBoard, 'computer');
     // while (!this.#gameOver) {
     // player/computer turn
