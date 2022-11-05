728e97f62a04d2019a19850b5bf5552959129664
Daniel Hjartland <squashical@gmail.com>
2022-11-05 16:12:58 +0100

    Added a standard fleet to Player class

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,9 +1,22 @@
+import Ship from "./Ship";
+
 class Player {
   constructor(name = 'Player') {
     this.name = name;
+    this.shipFleet = Player.#generateFleet();
   }
   attack(board, x, y) {
     board.receiveAttack(x, y);
+  }
+
+  static #generateFleet() {
+    return [
+      new Ship('Carrier', 5),
+      new Ship('Battleship', 4),
+      new Ship('Cruiser', 3),
+      new Ship('Submarine', 3),
+      new Ship('Destroyer', 2),
+    ];
   }
 }
 
