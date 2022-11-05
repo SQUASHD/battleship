fc4ed1a83ff3ceeecf51d96d1f5c213267a0c0a9
Daniel Hjartland <squashical@gmail.com>
2022-11-05 13:17:14 +0100

    Linting

--- src/test/UI.test.js
+++ src/test/UI.test.js
@@ -5,7 +5,7 @@ import UI from '../modules/UI';
 test('UI reference boards empty when ships not placed', () => {
   const playerBoard = new Gameboard(10);
   const computerBoard = new Gameboard(10);
-  const userInterface = new UI(playerBoard, computerBoard);
+  const userInterface = new UI();
   expect(
     userInterface.playerReferenceBoard.every((row) => row.every((space) => space === null))
   ).toBe(true);
@@ -19,10 +19,15 @@ test('UI reference boards accurately maps to actual gameboard', () => {
   playerBoard.placeShip(new Ship('Carrier', 5), 0, 0, 'horizontal');
   computerBoard.placeShip(new Ship('Carrier', 5), 0, 0, 'horizontal');
   const userInterface = new UI(playerBoard, computerBoard);
-  playerBoard.receiveAttack(0, 0)
-  computerBoard.receiveAttack(0, 0)
+  playerBoard.receiveAttack(0, 0);
+  computerBoard.receiveAttack(0, 0);
   userInterface.updateReferenceBoard(playerBoard.board, userInterface.playerReferenceBoard, 0, 0);
-  userInterface.updateReferenceBoard(computerBoard.board, userInterface.computerReferenceBoard, 0, 0);
+  userInterface.updateReferenceBoard(
+    computerBoard.board,
+    userInterface.computerReferenceBoard,
+    0,
+    0
+  );
   expect(userInterface.playerReferenceBoard[0][0]).toBe('hit');
   expect(userInterface.computerReferenceBoard[0][0]).toBe('hit');
 });
