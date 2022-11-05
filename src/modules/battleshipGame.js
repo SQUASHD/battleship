25b638059453161e29e340d6498bb82b84472d61
Daniel Hjartland <squashical@gmail.com>
2022-11-05 11:15:39 +0100

    Add function to place ships on board with predetermined coordinates

--- src/modules/battleshipGame.js
+++ src/modules/battleshipGame.js
@@ -16,7 +16,20 @@ class Game {
   isGameOver() {
     return this.#gameOver;
   }
-
+  placePlayerShips() {
+    this.playerBoard.placeShip(new Ship("Carrier", 5), 0, 0, "horizontal");
+    this.playerBoard.placeShip(new Ship("Battleship", 4), 0, 1, "horizontal");
+    this.playerBoard.placeShip(new Ship("Cruiser", 3), 0, 2, "horizontal");
+    this.playerBoard.placeShip(new Ship("Submarine", 3), 0, 3, "horizontal");
+    this.playerBoard.placeShip(new Ship("Destroyer", 2), 0, 4, "horizontal");
+  }
+  placeComputerShips() {
+    this.computerBoard.placeShip(new Ship("Carrier", 5), 0, 0, "horizontal");
+    this.computerBoard.placeShip(new Ship("Battleship", 4), 0, 1, "horizontal");
+    this.computerBoard.placeShip(new Ship("Cruiser", 3), 0, 2, "horizontal");
+    this.computerBoard.placeShip(new Ship("Submarine", 3), 0, 3, "horizontal");
+    this.computerBoard.placeShip(new Ship("Destroyer", 2), 0, 4, "horizontal");
+  }
 }
 
 export default Game;
