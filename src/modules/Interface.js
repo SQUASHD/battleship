7d20981559c654df6020d6145e8ea8f13babfb09
Daniel Hjartland <squashical@gmail.com>
2022-11-05 12:22:20 +0100

    Create Interface class

---
+++ src/modules/Interface.js
@@ -0,0 +1,13 @@
+import Gameboard from "./gameboard";
+
+class Interface {
+  constructor(playerBoard, computerBoard) {
+    this.playerReferenceBoard = playerBoard;
+    this.computerReferenceBoard = computerBoard;
+  }
+  renderBoard(board) {
+    
+  }
+}
+
+export default Interface
