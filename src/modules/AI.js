8425aa2bde15cc06a9d8b79a3ba07ca8390dc798
Daniel Hjartland <squashical@gmail.com>
2022-11-05 09:30:05 +0100

    Create AI class with accompanying tests

---
+++ src/modules/AI.js
@@ -0,0 +1,38 @@
+class AI {
+  constructor(name = 'Computer', currentBoardSize = 7) {
+    this.name = name;
+    this.moveList = AI.generateMoveList(currentBoardSize);
+  }
+  static generateMoveList(boardSize) {
+    const moveList = [];
+    for (let i = 0; i < boardSize; i++) {
+      moveList.push([]);
+      for (let j = 0; j < boardSize; j++) {
+        moveList[i].push('none');
+      }
+    }
+    return moveList;
+  }
+
+  selectRandomSquare() {
+    if (noMoreMoves(this.moveList)) {
+      return null;
+    }
+    
+    let x = Math.floor(Math.random() * this.moveList.length);
+    let y = Math.floor(Math.random() * this.moveList.length);
+    while (this.moveList[x][y] !== 'none') {
+      x = Math.floor(Math.random() * this.moveList.length);
+      y = Math.floor(Math.random() * this.moveList.length);
+    }
+    this.moveList[x][y] = 'move-made';
+
+    return [x, y];
+
+    function noMoreMoves(moveList) {
+      return moveList.every(row => row.every(space => space !== 'none'));
+    }
+  }
+}
+
+export default AI;
---
+++ src/test/AI.test.js
@@ -0,0 +1,71 @@
+import AI from '../modules/AI';
+
+describe('AI functions and attributes', () => {
+  let computer;
+  beforeEach(() => {
+    computer = new AI();
+  });
+  it('has a default name', () => {
+    expect(computer.name).toEqual('Computer');
+  });
+  it('can have a custom name', () => {
+    let newcomputer = new AI('The Admiral');
+    expect(newcomputer.name).toEqual('The Admiral');
+  });
+  it('has a moveList', () => {
+    expect(computer.moveList).toBeDefined();
+  });
+  it('has a moveList with only "none" at start', () => {
+    expect(computer.moveList).toEqual([
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+    ]);
+  });
+  it('can select a random square', () => {
+    expect(computer.selectRandomSquare()).toBeDefined();
+  });
+  it('can select a random square that is not already selected', () => {
+    let sampleMoveList = [
+      ['none', 'move-made', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+      ['none', 'none', 'none', 'none', 'none', 'none', 'none'],
+    ];
+    computer.moveList = sampleMoveList;
+    expect(computer.selectRandomSquare()).not.toEqual([0, 1]);
+  });
+  it('selects the last available square if all others are taken', () => {
+    let sampleMoveList = [
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'none'],
+    ];
+    computer.moveList = sampleMoveList;
+    expect(computer.selectRandomSquare()).toEqual([6, 6]);
+  });
+  it('returns null if no more moves are available', () => {
+    let sampleMoveList = [
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+      ['move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made', 'move-made'],
+    ];
+    computer.moveList = sampleMoveList;
+    expect(computer.selectRandomSquare()).toBeNull();
+  });
+});
