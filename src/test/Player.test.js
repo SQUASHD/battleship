25cbe9385023324a3a54c3c81c5c4931f78701d2
Daniel Hjartland <squashical@gmail.com>
2022-11-05 15:19:59 +0100

    Add test for attack method

--- src/test/Player.test.js
+++ src/test/Player.test.js
@@ -1,6 +1,9 @@
+import AI from '../modules/AI';
 import Player from '../modules/Player';
+import Gameboard from '../modules/gameboard';
+import Ship from '../modules/Ship';
 
-describe('Player functions and attributes', () => {
+describe('Player functions and attributes at start', () => {
   let player;
   beforeEach(() => {
     player = new Player();
@@ -11,5 +14,22 @@ describe('Player functions and attributes', () => {
   it('can have a custom name', () => {
     player = new Player('Bob');
     expect(player.name).toEqual('Bob');
+  });
+});
+describe('Player functions during game', () => {
+  let player;
+  let playerBoard;
+  let computer;
+  let computerBoard;
+  beforeEach(() => {
+    player = new Player();
+    playerBoard = new Gameboard();
+    computer = new AI();
+    computerBoard = new Gameboard();
+  });
+  it('can attack the computer', () => {
+    computerBoard.placeShip(new Ship('Carrier', 5), 0, 0, 'horizontal');
+    player.attack(computerBoard, 0, 0);
+    expect(computerBoard.board[0][0].hits).toBe(1);
   });
 });
